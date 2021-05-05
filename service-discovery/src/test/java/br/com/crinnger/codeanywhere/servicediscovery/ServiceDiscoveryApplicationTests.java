fd90b7f495fd454d026e18321f2ab84a5d2d239a
Crinnger <crinnger@gmail.com>
2021-05-05 11:16:06 -0400

    Atualizando configuracao

---
+++ service-discovery/src/test/java/br/com/crinnger/codeanywhere/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crinnger.codeanywhere.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
